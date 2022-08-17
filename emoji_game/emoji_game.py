c0eb34a8b6620931f1d1d2d46638971796689e02
Isabelle Dias <isabelle.dias@industriall.ai>
2022-08-17 09:10:08 -0300

    🤡 FEAT: Country edition of emoji game

--- emoji_game/emoji_game.py
+++ emoji_game/emoji_game.py
@@ -1,13 +1,23 @@
 import random
-from .emoji_dict import movie_dict
+from .emoji_dict import movie_dict, country_dict
 
 class EmojiGame():
     def show_movie_options():
         for name, value in movie_dict.items():
             print(f'{name} : {value}')
 
+    def show_country_options():
+        for name, value in country_dict.items():
+            print(f'{name} : {value}')
+
     def movie_edition():
         name, hint = random.choice(list(movie_dict.items()))
+        print(hint)
+        input('Press ENTER to get answer.')
+        print(name)
+
+    def country_edition():
+        name, hint = random.choice(list(country_dict.items()))
         print(hint)
         input('Press ENTER to get answer.')
         print(name)
